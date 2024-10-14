2eb596b962b72d5ba7be414e8e5af142626cac9f
MynnTapp <Desmynnmjc@gmail.com>
2024-10-14 11:38:20 -0500

    finished phase 3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -90,9 +90,53 @@
 
 // export default App;
 
+// import { useState, useEffect } from "react";
+// import { useDispatch } from "react-redux";
+// import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+// import Navigation from "./components/Navigation/Navigation";
+// import * as sessionActions from "./store/session";
+
+// function Layout() {
+//   const dispatch = useDispatch();
+//   const [isLoaded, setIsLoaded] = useState(false);
+
+//   useEffect(() => {
+//     dispatch(sessionActions.restoreUser()).then(() => {
+//       setIsLoaded(true);
+//     });
+//   }, [dispatch]);
+
+//   return (
+//     <>
+//       <Navigation isLoaded={isLoaded} />
+//       {isLoaded && <Outlet />}
+//     </>
+//   );
+// }
+
+// const router = createBrowserRouter([
+//   {
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: "/",
+//         element: <h1>Welcome!</h1>,
+//       },
+//     ],
+//   },
+// ]);
+
+// function App() {
+//   return <RouterProvider router={router} />;
+// }
+
+// export default App;
+
 import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { Outlet, createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage/LoginFormPage";
+import SignupFormPage from "./components/SignupFormPage/SignupFormPage";
 import Navigation from "./components/Navigation/Navigation";
 import * as sessionActions from "./store/session";
 
@@ -121,6 +165,14 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <h1>Welcome!</h1>,
+      },
+      {
+        path: "login",
+        element: <LoginFormPage />,
+      },
+      {
+        path: "signup",
+        element: <SignupFormPage />,
       },
     ],
   },
