d37de2d1f6c84fb683a98eb36ebba08376586e81
MynnTapp <Desmynnmjc@gmail.com>
2024-10-14 12:25:30 -0500

    finished phase 4

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,12 @@
 // import React from "react";
 // import ReactDOM from "react-dom/client";
-// import { Provider } from "react-redux";
 // import App from "./App";
 // import "./index.css";
+// import { Provider } from "react-redux";
 // import configureStore from "./store/store";
 // import { restoreCSRF, csrfFetch } from "./store/csrf";
 // import * as sessionActions from "./store/session";
-// import { RouterProvider } from "react-router-dom";
-// import router from "./App";
-//import { login, restoreUser, removeUser } from "./store/session";
+// import { Modal, ModalProvider } from "./context/Modal";
 
 // const store = configureStore();
 
@@ -20,16 +18,14 @@
 //   window.sessionActions = sessionActions;
 // }
 
-// window.sessionActions = { login, restoreUser, removeUser };
-
-// store.dispatch(restoreUser());
-
 // ReactDOM.createRoot(document.getElementById("root")).render(
 //   <React.StrictMode>
-//     <Provider store={store}>
-//       {/* <RouterProvider route={router} /> */}
-//       <App />
-//     </Provider>
+//     <ModalProvider>
+//       <Provider store={store}>
+//         <App />
+//         <Modal />
+//       </Provider>
+//     </ModalProvider>
 //   </React.StrictMode>
 // );
 
@@ -41,7 +37,7 @@ import { Provider } from "react-redux";
 import configureStore from "./store/store";
 import { restoreCSRF, csrfFetch } from "./store/csrf";
 import * as sessionActions from "./store/session";
-//import { Modal, ModalProvider } from "./context/Modal";
+import { Modal, ModalProvider } from "./context/Modal";
 
 const store = configureStore();
 
@@ -55,8 +51,11 @@ if (import.meta.env.MODE !== "production") {
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <ModalProvider>
+      <Provider store={store}>
+        <App />
+        <Modal />
+      </Provider>
+    </ModalProvider>
   </React.StrictMode>
 );
